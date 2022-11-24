e980eb2e89b0650c5da148b4ebd254f84566c080
aq2 <angelo@fluxton.co.uk>
2022-11-24 15:35:18 +0000

    initial commitial

---
+++ components/NavBar.vue
@@ -0,0 +1,41 @@
+<template lang='pug' 🕷️>
+
+nav
+  .tab#welcome Welcome
+  .tab#treatments Treatments
+  .tab#training Massage Training
+  .tab#yoga Yoga Classes
+  .tab#joyful Joyful Living
+  .tab#dosha Dosha Principles
+  .tab#contact Contact
+
+  p hi
+
+</template> 🕷️
+
+
+<script setup 🚀>
+
+
+
+</script> 🚀
+
+
+<style lang='stylus' scoped  🐯>
+@import '../stylus/typography'
+
+
+nav
+  display flex
+  flex-wrap wrap
+  justify-content space-between
+
+nav .tab
+  flex-grow 1
+  padding 1rem
+  cursor pointer
+  text-align center
+  border 2px solid orange
+  background $green
+
+</style> 🐯
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template lang='pug' 🕷️>
+
+header
+  h1 Ayurvedic Yoga Massage
+
+NavBar
+
+main
+  aside
+    p image
+  section
+    p welcome
+
+
+</template> 🕷️
+
+
+<script setup 🚀>
+
+
+
+</script> 🚀
+
+
+<style lang='stylus' 🐯>
+
+@import '../stylus/typography'
+
+header
+  background $teal
+
+
+</style> 🐯
